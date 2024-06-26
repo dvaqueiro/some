d2f8d10aa4f217eb6b0c616c52708e436d27dbcb
dvaqueiro <dvaqueiro@boardfy.com>
2024-06-26 13:14:26 +0200

    server port argument

--- src/some.c
+++ src/some.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "server.h"
 
-#define PORT 8008
 #define BACKLOG 5
 
-int main() {
+int main(int argc, char **argv) {
     server_t *server;
 
-    server = server_new(PORT, BACKLOG);
+    int opt, port = 8008;
+
+    while ((opt = getopt(argc, argv, "p:v")) != -1) {
+        switch (opt) {
+            case 'p':
+                port = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-p port]\n", argv[0]);
+                exit(1);
+        }
+    }
+
+    server = server_new(port, BACKLOG);
     server_run(server);
     printf("Server gracefull stopped\n");
 
